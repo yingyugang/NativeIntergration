7b8b06d9c7d52ab83609656cac27c72dcc54b501
removedporn <86824510+removedporn@users.noreply.github.com>
2022-10-17 17:51:43 +0800

    Delete Assets directory

--- Assets/Plugins/Android/IntergrationInterface.java
+++
@@ -1,7 +0,0 @@
-package com.unity.intergration;
-
-public interface IntergrationInterface {
-    void Callback(int result);
-    void Callback(String result);
-    void Callback(NativeParameter nativeParameter);
-}
